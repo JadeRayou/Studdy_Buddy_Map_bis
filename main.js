c1995d200daaa9379e4f71c799614d614ad43721
Jade Rayou <125276833+JadeRayou@users.noreply.github.com>
2023-05-02 12:21:20 +0200

    debug de la map

--- main.js
+++ main.js
@@ -9,23 +9,10 @@ var modal = document.querySelector('#laModale');
 var inputTitre = document.querySelector('#titre');
 var inputImage = document.querySelector('#image');
 var inputInfo = document.querySelector('#info');
-var filterMusées = document.querySelector('#filterMusées')
-var filterAssociations = document.querySelector('filterAssociations');
 var coordonnée;
-var tableauMarker ;
+var tableauMarker;
 var radios = document.getElementsByName('filterForMap');
 var valeur;
-
-// ajouter une icon couleur
-var newicon = new L.Icon({
-    iconUrl: 'img/location-pin.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: [50, 50],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-})
-
 
 try {
     // on essaye de recuperer le tableau dans le localstorage
@@ -37,53 +24,39 @@ catch (error) {
     // et on ne fait rien avec l'erreur
 }
 
-// fonction pour les boutons des filtres sur la modal
-function returnMuseesOrAssociations(){
-	if(document.getElementById('filterMusées').checked){
-		var valeur = document.getElementById('filterMusées').value;
-	} else { // si ce n'est pas l'un, c'est l'autre
-		var valeur = document.getElementById('filterAssociations').value;
-	}
-	return valeur;
-}
-
 // on enregistre les coordonnées du click et on affiche la modale
 function onMapClick(e) {
-    coord = e.latlng;
-    var marker = new L.Marker([e.latlng.lat, e.latlng.lng], {icon: newicon}).addTo(map);
+    coordonnée = e.latlng;
     modal.showModal();
-}
-map.on('click', onMapClick);
+};
 
-// on enregistre les infos rentrées dans la modale sous forme d'objet pour chaque marker et on l'enregistre dans le localStorage
+// lors de l'ajout du marker via la modale on enregistre les données rentrées dans un objet
+// que l'on enregistre dans le localStorage et on vide les champs de la modale
+map.on('click', onMapClick);
 modal.addEventListener('close', function () {
+    console.log(modal.returnValue)
     if (modal.returnValue === 'oui') {
         tableauMarker.push({
-            titre : inputNom.value,
-            image : inputImage.value,
-            info : inputInfo.value,
-            coordonnée : coordonnée,
-            valeur : returnMuseesOrAssociations(),
+            titre: inputTitre.value,
+            image: inputImage.value,
+            info: inputInfo.value,
+            coordonnée: coordonnée
         });
-        // Update local storage
         localStorage.setItem('savetableauMarker_v2', JSON.stringify(tableauMarker));
-        ajouterMarker(inputNom.value,inputImage.value, inputInfo.value, coordonnée, valeur )
-        // on vide les champs de la modale
+        ajoutMarkerSurLaMap(inputTitre.value, inputImage.value, inputInfo.value, coordonnée);
         inputNom.value = "";
         inputImage.value = "";
         inputInfo.value = "";
     }
 });
 
-// on ajoute le marker sur la map avec le popup qui correspond
+// on charge les marqueurs du localstorage
+for (var i = 0; i < tableauMarker.length; i++) {
+    ajoutMarkerSurLaMap(tableauMarker[i].titre, tableauMarker[i].image, tableauMarker[i].info, tableauMarker[i].coordonnée);
+}
+
+// on ajoute le marker sur la map avec le popup associé
 function ajoutMarkerSurLaMap(titre, image, info, coordonnée) {
-    // Icon options
-    // var iconOptions = {
-    //     iconUrl: 'images/komarov-egor-sYwDefjO7fI-unsplash.jpg',
-    //     iconSize: [60, 50]
-    // }
-    // // Creating a custom icon
-    // var customIcon = L.icon(iconOptions);
     var marker = new L.Marker([coordonnée.lat, coordonnée.lng],{ draggable: true }).addTo(map);
     marker.bindPopup(
         '<h2>' + titre + '</h2>'
@@ -93,28 +66,14 @@ function ajoutMarkerSurLaMap(titre, image, info, coordonnée) {
     );
 }
 
-// fonction pour déplacer le marker 
+// fonction pour déplacer le marker
 marker.on('dragend', function(event) {
     var marker = event.target;
     var position = marker.getLatLng();
     console.log(position);
-   
 });
 
-// on charge les markers du localstorage
-for (var i = 0; i < tableauMarker.length; i++) {
-    ajoutMarkerSurLaMap(tableauMarker[i].titre, tableauMarker[i].image, tableauMarker[i].info, tableauMarker[i].coordonnée);
-}
-
-// fonction pour déplacer le marker 
-marker.on('dragend', function(event) {
-    var marker = event.target;
-    var position = marker.getLatLng();
-    console.log(position);
-   
-});
-
-// supprimer un marker
+// on supprime les markers
 function supprimeMarker(lat, lng) {
     map.eachLayer(function (layer) {
         if (layer instanceof L.Marker) {
@@ -123,9 +82,8 @@ function supprimeMarker(lat, lng) {
             }
         }
     });
-    // test
     tableauMarker = tableauMarker.filter(function (marker) {
         return marker.coordonnée.lat !== lat || marker.coordonnée.lng !== lng;
     });
-    localStorage.setItem('savetableauMarker_v2', JSON.stringify(tableauMarker))
+    localStorage.setItem('savetableauMarker_v2', JSON.stringify(tableauMarker));
 }
